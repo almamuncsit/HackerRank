5eea1485df3c79d0087f189c6053f81d478b54d7
Art 0f CSE <artofcse@gmail.com>
2019-12-03 22:21:08 +0600

    Solve math problems

---
+++ Python/05 Math/Polar-Coordinates.py
@@ -0,0 +1,6 @@
+import cmath
+
+input = complex( input() )
+
+print( abs(input) )
+print( cmath.phase(input) )
---
+++ Python/05 Math/Triangle-Quest-2.py
@@ -0,0 +1,2 @@
+for i in range(1,int(input())+1): 
+    print(pow( ((pow(10,i)-1)//9), 2))
