1c7c80d323d2bba492db3d22fd6f24262c42dc5e
Al-Mamun Sarkar <msarkar.cse@gmail.com>
2020-04-05 20:34:11 +0600

    Solve 2 sorting problem

---
+++ Interview Preparation Kit/sort/01-Sorting-Bubble-Sort.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countSwaps function below.
+def countSwaps(a):
+	swaps = 0
+	for i in range(len(a)):
+		for j in range(len(a)-1):
+			if a[j] > a[j+1]:
+				a[j], a[j+1] = a[j+1], a[j]
+				swaps += 1
+
+	print(f'Array is sorted in {swaps} swaps.')
+	print('First Element:', a[0])
+	print('Last Element:', a[-1])
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
