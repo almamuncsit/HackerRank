261c41b907a7cc3044f3d4a7696574dbaddfe63c
Al-Mamun Sarkar <msarkar.cse@gmail.com>
2020-07-22 13:40:31 +0600

    Update some codes

--- Algorithms/String/05-caesar-cipher-1.py
+++ Algorithms/String/05-caesar-cipher-1.py
@@ -8,28 +8,27 @@ import sys
 
 # Complete the caesarCipher function below.
 def caesarCipher(s, k):
-    s_list = list(map(ord, s))
+    temp = list(map(ord, s))
     k = k % 26
-    for i in range(len(s_list)):
-        if 65 <= s_list[i] <= 90:
-            s_list[i] = 65 + ((s_list[i] - 65) + k) % 26
-        elif 97 <= s_list[i] <= 122:
-            s_list[i] = 97 + ((s_list[i] - 97) + k) % 26
-    s_list = map(chr, s_list)    
-    return ''.join(s_list)
+    for i in range(len(temp)):
+        if 65 <= temp[i] <= 90:
+            temp[i] = 65 + ((temp[i] - 65) + k) % 26
+        elif 97 <= temp[i] <= 122:
+            temp[i] = 97 + ((temp[i] - 97) + k) % 26
+    temp = map(chr, temp)
+        
+    return ''.join(temp)
     
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input())
-
     s = input()
-
     k = int(input())
 
     result = caesarCipher(s, k)
     print(result)
+    
     # fptr.write(result + '\n')
-
     # fptr.close()
